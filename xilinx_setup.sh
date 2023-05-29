3edd40fa7cfb33644eaa851ba4317f10ca02d434
phuhv <van-phu.ha@inria.fr>
2023-05-30 00:31:36 +0200

    zedboard setup

---
+++ xilinx_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export BOARD=zedboard
+export XILINX_PART="xc7z020clg484-1"
+export XILINX_BOARD="em.avnet.com:zynq:zed:c"
+PATH=$PATH:/home/phuhv/tools/Xilinx/SDx/2017.2/Vivado/bin/:/home/phuhv/tools/Xilinx/SDx/2017.2/bin
+RISCY_RV32F=0
